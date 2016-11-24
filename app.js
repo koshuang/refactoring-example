9a182aa6e522e86312a6c7f7474958d1909da403
Kos <koshuang@gmail.com>
2016-11-24 17:32:32 +0800

    extract formatDate().

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 var format = 'short-1';
 var input = '2016/01/02 11:33:20';
-var result;
-
-switch (format) {
-  case 'short-1':
-    result = moment(input).format('MM/DD/YYYY');
-    break;
-  case 'short-2':
-    result = moment(input).format('MM/DD/YY');
-    break;
-  case 'short-3':
-    result = moment(input).format('MMMM DD, YYYY');
-    break;
-  case 'short-4':
-    result = moment(input).format('MMM. Do YYYY');
-    break;
-  case 'short-day':
-    result = moment(input).format('dddd');
-    break;
-  case 'long-1':
-    result = moment(input).format('dddd, MMM. Do YYYY, h:mm a');
-    break;
-  case 'long-2':
-    result = moment(input).format('MM/DD/YY, h:mm a');
-    break;
-  case 'long-3':
-    result = moment(input).format('MMMM DD YYYY,  h:mm a');
-    break;
-  case 'long-4':
-    result = moment(input).format('MM/DD/YYYY,  hh:mm');
-    break;
-  case 'referenceTime':
-    result = moment(input).calendar();
-    break;
-  case 'durationSec':
-    result = moment.duration(input, 'seconds').humanize();
-    break;
-  default:
-    break;
-}
+var result = formatDate(format, input);
 
 console.log(result);
+
+function formatDate(format, input) {
+  switch (format) {
+    case 'short-1':
+      result = moment(input).format('MM/DD/YYYY');
+      break;
+    case 'short-2':
+      result = moment(input).format('MM/DD/YY');
+      break;
+    case 'short-3':
+      result = moment(input).format('MMMM DD, YYYY');
+      break;
+    case 'short-4':
+      result = moment(input).format('MMM. Do YYYY');
+      break;
+    case 'short-day':
+      result = moment(input).format('dddd');
+      break;
+    case 'long-1':
+      result = moment(input).format('dddd, MMM. Do YYYY, h:mm a');
+      break;
+    case 'long-2':
+      result = moment(input).format('MM/DD/YY, h:mm a');
+      break;
+    case 'long-3':
+      result = moment(input).format('MMMM DD YYYY,  h:mm a');
+      break;
+    case 'long-4':
+      result = moment(input).format('MM/DD/YYYY,  hh:mm');
+      break;
+    case 'referenceTime':
+      result = moment(input).calendar();
+      break;
+    case 'durationSec':
+      result = moment.duration(input, 'seconds').humanize();
+      break;
+    default:
+      break;
+  }
+
+  return result;
+}
