dad05ae8ec1e726e8f03167f3f3ac2f80810888f
Kos <koshuang@gmail.com>
2016-12-08 18:23:03 +0800

    refactor: extract test data

--- test/app.test.js
+++ test/app.test.js
@@ -1,25 +1,11 @@
 var assert = require('assert');
 var app = require('../app.js');
+var records = require('./data/dateFormats/dateFormatMappings.js').records;
 
 describe('app.js', function() {
   describe('#formatDate()', function() {
     var formatDate = app.formatDate;
     var input = '2016/01/02 11:33:20';
-
-    var records = [
-      {
-        type: 'short-1',
-        expectation: '01/02/2016',
-      },
-      {
-        type: 'short-2',
-        expectation: '01/02/16',
-      },
-      {
-        type: 'short-3',
-        expectation: 'January 02, 2016',
-      },
-    ];
 
     records.forEach(function(record) { // => destrcuting
       it('should return ' + record.expectation + ' when the input is ' + record.type, function() {
---
+++ test/data/dateFormats/dateFormatMappings.js
@@ -0,0 +1,16 @@
+var records = [
+  {
+    type: 'short-1',
+    expectation: '01/02/2016',
+  },
+  {
+    type: 'short-2',
+    expectation: '01/02/16',
+  },
+  {
+    type: 'short-3',
+    expectation: 'January 02, 2016',
+  },
+];
+
+exports.records = records;
