b31dbfb2d31baf6649501c32a74b6cc0a2f2023f
Kos <koshuang@gmail.com>
2016-12-08 17:53:46 +0800

    refactor: add action property to mapping object.

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ function formatDate(input, type) {
   var keys = Object.keys(mapping);
 
   if (keys.indexOf(type) > -1) {
-    var dateFormat = mapping[type].dateFormat;
-    return formatMomentDate(input, dateFormat);
+    var obj = mapping[type];
+    var dateFormat = obj.dateFormat;
+    var action = obj.action;
+    return action(input, dateFormat);
   }
 
   switch (type) {
@@ -25,15 +27,15 @@ function formatMomentDate(input, dateFormat) {
 
 function getDateFomatMapping() {
   var obj = {
-    'short-1': { dateFormat: 'MM/DD/YYYY' },
-    'short-2': { dateFormat: 'MM/DD/YY' },
-    'short-3': { dateFormat: 'MMMM DD, YYYY' },
-    'short-4': { dateFormat: 'MMM. Do YYYY' },
-    'short-day': { dateFormat: 'dddd' },
-    'long-1': { dateFormat: 'dddd, MMM. Do YYYY, h:mm a' },
-    'long-2': { dateFormat: 'MM/DD/YY, h:mm a' },
-    'long-3': { dateFormat: 'MMMM DD YYYY,  h:mm a' },
-    'long-4': { dateFormat: 'MM/DD/YYYY,  hh:mm' },
+    'short-1':   { dateFormat: 'MM/DD/YYYY',                 action: formatMomentDate },
+    'short-2':   { dateFormat: 'MM/DD/YY',                   action: formatMomentDate },
+    'short-3':   { dateFormat: 'MMMM DD, YYYY',              action: formatMomentDate },
+    'short-4':   { dateFormat: 'MMM. Do YYYY',               action: formatMomentDate },
+    'short-day': { dateFormat: 'dddd',                       action: formatMomentDate },
+    'long-1':    { dateFormat: 'dddd, MMM. Do YYYY, h:mm a', action: formatMomentDate },
+    'long-2':    { dateFormat: 'MM/DD/YY, h:mm a',           action: formatMomentDate },
+    'long-3':    { dateFormat: 'MMMM DD YYYY,  h:mm a',      action: formatMomentDate },
+    'long-4':    { dateFormat: 'MM/DD/YYYY,  hh:mm',         action: formatMomentDate },
   };
 
   return obj;
