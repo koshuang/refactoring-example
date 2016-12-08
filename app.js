c588c26b713e285780c4013706e21bbf829cb2c0
Kos <koshuang@gmail.com>
2016-12-08 18:02:26 +0800

    refactor: extract formatMomentDate() and formatMomentCalendar()

--- app.js
+++ app.js
@@ -11,31 +11,34 @@ function formatDate(input, type) {
     return action(input, dateFormat);
   }
 
-  switch (type) {
-    case 'referenceTime':
-      return moment(input).calendar();
-    case 'durationSec':
-      return moment.duration(input, 'seconds').humanize();
-    default:
-      return input;
-  }
+  return input;
 }
 
 function formatMomentDate(input, dateFormat) {
   return moment(input).format(dateFormat);
 }
 
+function formatMomentCalendar(input) {
+  return moment(input).calendar();
+}
+
+function formatMomentDuration(input) {
+  return moment.duration(input, 'seconds').humanize();
+}
+
 function getDateFomatMapping() {
   var obj = {
-    'short-1':   { dateFormat: 'MM/DD/YYYY',                 action: formatMomentDate },
-    'short-2':   { dateFormat: 'MM/DD/YY',                   action: formatMomentDate },
-    'short-3':   { dateFormat: 'MMMM DD, YYYY',              action: formatMomentDate },
-    'short-4':   { dateFormat: 'MMM. Do YYYY',               action: formatMomentDate },
-    'short-day': { dateFormat: 'dddd',                       action: formatMomentDate },
-    'long-1':    { dateFormat: 'dddd, MMM. Do YYYY, h:mm a', action: formatMomentDate },
-    'long-2':    { dateFormat: 'MM/DD/YY, h:mm a',           action: formatMomentDate },
-    'long-3':    { dateFormat: 'MMMM DD YYYY,  h:mm a',      action: formatMomentDate },
-    'long-4':    { dateFormat: 'MM/DD/YYYY,  hh:mm',         action: formatMomentDate },
+    'short-1':       { dateFormat: 'MM/DD/YYYY',                 action: formatMomentDate },
+    'short-2':       { dateFormat: 'MM/DD/YY',                   action: formatMomentDate },
+    'short-3':       { dateFormat: 'MMMM DD, YYYY',              action: formatMomentDate },
+    'short-4':       { dateFormat: 'MMM. Do YYYY',               action: formatMomentDate },
+    'short-day':     { dateFormat: 'dddd',                       action: formatMomentDate },
+    'long-1':        { dateFormat: 'dddd, MMM. Do YYYY, h:mm a', action: formatMomentDate },
+    'long-2':        { dateFormat: 'MM/DD/YY, h:mm a',           action: formatMomentDate },
+    'long-3':        { dateFormat: 'MMMM DD YYYY,  h:mm a',      action: formatMomentDate },
+    'long-4':        { dateFormat: 'MM/DD/YYYY,  hh:mm',         action: formatMomentDate },
+    'referenceTime': { dateFormat: null,                         action: formatMomentCalendar },
+    'durationSec':   { dateFormat: null,                         action: formatMomentDuration },
   };
 
   return obj;
